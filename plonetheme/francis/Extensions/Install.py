d7ddc8813535e7cf63e6f5924040dcd7b1721dfd
Brian Davis <fosten@gmail.com>
2013-12-12 10:31:51 -0500

    Added uninstall profile

--- plonetheme/francis/Extensions/Install.py
+++ plonetheme/francis/Extensions/Install.py
@@ -1,26 +1,11 @@
 from Products.CMFCore.utils import getToolByName
 
-THEMENAME='francis theme'
+def install(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-plonetheme.francis:default')
+    return "Ran all import steps."
 
 def uninstall(portal):
-    """Uninstall by cleaning up portal_skins"""
-    skins_tool = getToolByName(portal, 'portal_skins')
-
-    # Try to clean up
-    if THEMENAME in skins_tool['selections']:
-        del skins_tool['selections'][THEMENAME]
-
-    # Set a reasonable default skin
-    if skins_tool.getDefaultSkin() == THEMENAME:
-        skins = skins_tool.getSkinSelections()
-        default_skins = ['Sunburst Theme', 'Plone Classic Theme', 'Plone Default']
-        for ds in default_skins:
-            if ds in skins:
-                skins_tool.default_skin = ds
-                break
-        if skins_tool.getDefaultSkin() == THEMENAME:
-            return "%s uninstalled, but unable to set a default skin." % THEMENAME
-
-        return "%s uninstalled; default skin set to %s." % (THEMENAME, ds)
-    return "%s uninstalled." % THEMENAME
-
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-plonetheme.francis:uninstall')
+    return "Ran all uninstall steps."
