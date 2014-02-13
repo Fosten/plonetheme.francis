943184ec4470b34fc1c58ce5d7c784b6cae981d3
Brian Davis <fosten@gmail.com>
2014-02-13 10:16:34 -0500

    Revert "Added uninstall profile"
    This reverts commit d7ddc8813535e7cf63e6f5924040dcd7b1721dfd.

--- plonetheme/francis/Extensions/Install.py
+++ plonetheme/francis/Extensions/Install.py
@@ -1,11 +1,26 @@
 from Products.CMFCore.utils import getToolByName
 
-def install(portal):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-plonetheme.francis:default')
-    return "Ran all import steps."
+THEMENAME='francis theme'
 
 def uninstall(portal):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-plonetheme.francis:uninstall')
-    return "Ran all uninstall steps."
+    """Uninstall by cleaning up portal_skins"""
+    skins_tool = getToolByName(portal, 'portal_skins')
+
+    # Try to clean up
+    if THEMENAME in skins_tool['selections']:
+        del skins_tool['selections'][THEMENAME]
+
+    # Set a reasonable default skin
+    if skins_tool.getDefaultSkin() == THEMENAME:
+        skins = skins_tool.getSkinSelections()
+        default_skins = ['Sunburst Theme', 'Plone Classic Theme', 'Plone Default']
+        for ds in default_skins:
+            if ds in skins:
+                skins_tool.default_skin = ds
+                break
+        if skins_tool.getDefaultSkin() == THEMENAME:
+            return "%s uninstalled, but unable to set a default skin." % THEMENAME
+
+        return "%s uninstalled; default skin set to %s." % (THEMENAME, ds)
+    return "%s uninstalled." % THEMENAME
+
